233109482172e03b99cff76adc63641db36d9259
Amit Kumar <amit.kumar8@gmail.com>
2020-08-11 15:03:25 +0530

    grid code + collapsce done

--- src/app/components/applicant-monitoring/applicant-monitoring.component.ts
+++ src/app/components/applicant-monitoring/applicant-monitoring.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicantMonitoringComponent implements OnInit {
 
+  gridData: any = [];
+  //panelOpenState: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initializePage();
   }
+
+  initializePage() {
+    this.gridData = [
+      {'name':'Application','id':'123234345','nameCrop':'Crop','type':'Pink Strawberry','aStatus':'Application Status update','progress':'3','panelOpenState':false},
+      {'name':'Application','id':'123234345','nameCrop':'Crop','type':'Pink Strawberry','aStatus':'Application Status update','progress':'3','panelOpenState':false},
+      {'name':'Application','id':'123234345','nameCrop':'Crop','type':'Pink Strawberry','aStatus':'Application Status update','progress':'3','panelOpenState':false}
+    ]
+  }
+
+  togglePanel(index) {
+    this.gridData[index].panelOpenState = !this.gridData[index].panelOpenState;
+}
 
 }
